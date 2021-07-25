712a5bb7bb93e3b893e51d40816a2f7564dd1aaa
apterek <danialsud@gmail.com>
2021-07-25 22:45:08 +0300

    up

---
+++ lesson_04/solution_01.py
@@ -0,0 +1,5 @@
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for value in my_list:
+    if value < 5:
+        print(value)
---
+++ lesson_04/solution_02.py
@@ -0,0 +1,6 @@
+line = input('Enter line: ')
+
+if line == line[::-1]:
+    print("it's polindrom")
+else:
+    print("it's not polindrom")
