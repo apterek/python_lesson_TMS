388194a1c423197472d8dca1d2112c70527d4fbf
apterek <danialsud@gmail.com>
2021-07-28 11:23:13 -0700

    up

--- lesson_06/solution_class_04.py
+++ lesson_06/solution_class_04.py
@@ -4,12 +4,16 @@ import random
 cards = [
     '6', '7', '8', '9', '10', 'J', 'D', 'K', 'A'
 ]
+
 suits = [
     'Hearts', 'Diamonds', 'Clubs', 'Spades'
 ]
-cards_value = {'6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 2, 'D': 3, 'K': 4, 'A': 11}
+cards_value = {
+    '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 2, 'D': 3, 'K': 4, 'A': 11
+}
 
 
+# generate random card
 def pull_random_card(cards_sequences, suit_sequences):
     random_card = {cards_sequences[random.randint(0, len(cards_sequences) - 1)]: suit_sequences[
         random.randint(0, len(suit_sequences) - 1)]}
@@ -18,9 +22,11 @@ def pull_random_card(cards_sequences, suit_sequences):
 
 def black_jack():
     sum_cards = 0
+    # Start game
     while True:
         pull_card = {}
         last_card = {}
+        # pull card
         while True:
             pull_card = pull_random_card(cards, suits)
             if pull_card == last_card:
@@ -28,15 +34,24 @@ def black_jack():
             else:
                 last_card = pull_card
                 break
+        # take card
         card = cards_value[list(pull_card.keys())[0]]
+        # check value card A
         if card == 11 and sum_cards+11 > 21:
             card = 1
+        # score
         sum_cards += card
         print(f'You scored {sum_cards} points')
+        # if score > max value
+        if sum_cards > 21:
+            return print(f'you score {sum_cards}, game end, YOU LOSE !!!')
+        elif sum_cards == 21:
+            return print(f'you score {sum_cards}, game end')
+        # pull more card
         game_continue = input('Enter | yes |  if you want to take another card: ')
-        if game_continue != 'yes' or sum_cards >= 21:
-            print(f'you score {sum_cards}, game end')
-            break
+        # check game continue
+        if game_continue != 'yes':
+            return print(f'you score {sum_cards}, game end')
 
 
 black_jack()
