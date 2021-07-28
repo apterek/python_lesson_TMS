e07f7e8cff2cf082be01e2a0a1e4fc029aacfecb
apterek <danialsud@gmail.com>
2021-07-28 11:05:09 -0700

    up

---
+++ lesson_07/solution_class_01.py
@@ -0,0 +1,17 @@
+import random
+
+cards = [
+    '6', '7', '8', '9', '10', 'J', 'D', 'K', 'A'
+]
+suit = [
+    'Hearts', 'Diamonds', 'Clubs', 'Spades'
+]
+
+
+def pull_random_card(cards_sequences, suit_sequences):
+    random_card = {cards_sequences[random.randint(0, len(cards_sequences) - 1)]: suit_sequences[
+        random.randint(0, len(suit_sequences) - 1)]}
+    return random_card
+
+
+print(pull_random_card(cards, suit))
--- lesson_07/solution_class_02.py
+++ lesson_07/solution_class_02.py
@@ -19,16 +19,7 @@ def pull_random_card(cards_sequences, suit_sequences):
 def black_jack():
     sum_cards = 0
     while True:
-        pull_card = {}
-        last_card = {}
-        while True:
-            pull_card = pull_random_card(cards, suits)
-            if pull_card == last_card:
-                continue
-            else:
-                last_card = pull_card
-                break
-        card = cards_value[list(pull_card.keys())[0]]
+        card = cards_value[list(pull_random_card(cards, suits).keys())[0]]
         if card == 11 and sum_cards+11 > 21:
             card = 1
         sum_cards += card
