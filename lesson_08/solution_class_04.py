debabd1145b0931ceecdcde10e74c98632f25465
Danila Sudnikovich <50168132+apterek@users.noreply.github.com>
2021-08-02 22:35:55 +0300

    Updated
    Updated

--- lesson_08/solution_class_04.py
+++ lesson_08/solution_class_04.py
@@ -1,7 +1,12 @@
 def numbers(start_number, end_number):
-    end_number =- 1
-    if end_number == start_number:
-        return print('Finish')
+    while True:
+        if end_number % end_number == 0 and end_number >= 1:
+            if end_number == 1:
+                return print(1)
+            print(end_number)
+            break
+        else:
+           end_number -= 1
     numbers (start_numbera, end_numbers)
 
 
