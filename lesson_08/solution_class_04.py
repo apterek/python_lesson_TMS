dc33a5fe97eac0d80edb80f6f9a2aeedc8351473
Danila Sudnikovich <50168132+apterek@users.noreply.github.com>
2021-08-02 22:40:36 +0300

    Up
    Up

--- lesson_08/solution_class_04.py
+++ lesson_08/solution_class_04.py
@@ -1,14 +1,15 @@
-def numbers(start_number, end_number):
+def numbers(end_number):
     while True:
         if end_number % end_number == 0 and end_number >= 1:
             if end_number == 1:
                 return print(1)
             print(end_number)
+            end_number -= 1
             break
         else:
            end_number -= 1
-    numbers (start_numbera, end_numbers)
+    numbers (end_number)
 
 
 if __name__ == '__main__':
-    number(0, 100)
+    numbers(100)
