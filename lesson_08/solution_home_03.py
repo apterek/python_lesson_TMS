d27cf2dff0fa919a8ecf4d83d0cff81a4d2d8495
apterek <danialsud@gmail.com>
2021-08-04 12:15:26 +0300

    update

--- lesson_08/solution_home_03.py
+++ lesson_08/solution_home_03.py
@@ -1,13 +1,48 @@
-q = 8
-from_1 = [1, 2, 3, 4, 5, 6, 7, 8]
-to_1 = []
-buf = []
+'''
+a_column = [1, 2, 3, 4, 5, 6, 7, 8]
+b_column = []
+c_column = []
+
+if not b_column:
+    print(b_column)
 
 
-def k(i, j, z, p):
-    if q == 0:
-        return print('finish')
+def num_check(number):
+    if number % 2 == 0:
+        return number
     else:
-        i -= 1
-        z = j[0]
+        return -1
 
+
+def transfer_by_column(_a, _b, _c):
+    _a = list(_a)
+    _b = list(_b)
+    _c = list(_c)
+    disk = 1
+    while True:
+        if _a[0] == 1:
+            _b.insert(0, 1)
+            _a.remove(1)
+        elif _c[0] > _a[0] == num_check(_a[0]):
+            _c.insert(0, _a[0])
+            _a.remove(_a[0])
+        elif _a[0] < _b[0]:
+            _b.insert(0, _a[0])
+            _a.remove(_a[0])
+        if not _b:
+            continue
+        elif _a[0] > _b[0] == num_check(_b[0]):
+            _a.insert(0, _b[0])
+            _b.remove(_b[0])
+        elif _a[0] < _b[0]:
+            _b.insert(0, _c[0])
+            _b.remove(_b[0])
+        if not _c:
+            continue
+        elif _b[0] > _c[0] == num_check(_c[0]):
+            _b.insert(0, _c[0])
+            _c.remove(_c[0])
+        elif _c[0] < _a[0]:
+            _a.insert(0, _c[0])
+            _c.remove(_c[0])
+'''
