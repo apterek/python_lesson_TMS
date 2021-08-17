18110eede12148a38f1359a69bb4ed843b577560
apterek <danialsud@gmail.com>
2021-08-17 17:48:46 +0300

    upss

--- lesson_12/homework/connection_module.py
+++ lesson_12/homework/connection_module.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy_utils import create_database, database_exists
 
 
@@ -6,9 +7,10 @@ def create_connect_to_db(user: str, password: str, db_name: str, address_db: int
     engine = create_engine(
         f"postgresql://{user}:{password}@{address_db}:{port_db}/{db_name}", echo=True
     )
+    session = Session(bind=engine)
     if not database_exists(engine.url):
         create_database(engine.url)
-        return print("Database create, continue... Connection secsfull!")
+        return session, print("Database create, continue... Connection secsfull!")
     else:
-        return print("Connection secsfull!")
+        return session, print("Connection secsfull!")
 
