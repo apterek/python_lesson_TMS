8effa32f4f7c1e61b1b264e9cfae61c7b6568638
apterek <danialsud@gmail.com>
2021-08-17 16:45:57 +0300

    up

---
+++ lesson_12/classwork.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy_utils import create_database, database_exists
+
+
+DB_USER = "danila"
+DB_PASSWORD = "271036yY#"
+DB_NAME = "pythondb"
+DB_ECHO = True
+DB_PORT = 54325
+
+
+def create_connect_to_db():
+    engine = create_engine(
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}", echo=True
+    )
+    engine.connect()
+    print(engine)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        return print('Database created')
+    else:
+        return print('Database already exist')
+
+
+create_connect_to_db()
